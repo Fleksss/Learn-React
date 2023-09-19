068e7ff255d8d13bf1d82b517971cf1bc177a00a
Abdukadir Samikjanov <abdukadirsamikjanov@gmail.com>
2023-09-19 16:14:39 +0500

    add Alert module, add modules file, add children props, add ReactNode

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,24 @@
-import ListGoup from "./components/ListGroup"
+// import ListGoup from "./components/ListGroup"
+import Alert from "./modules/Alert/views/Alert"
 
 import './App.css'
 
 function App() {
 
-  const cities = [
-    "New York",
-    "San Francisco",
-    "Tokyo",
-    "London",
-    "Paris"
-  ]
+  // const cities = [
+  //   "New York",
+  //   "San Francisco",
+  //   "Tokyo",
+  //   "London",
+  //   "Paris"
+  // ]
 
   return <div className="main">
-    <ListGoup cities={cities} heading="Cities" />
+    {/* <ListGoup cities={cities} heading="Cities" /> */
+      <Alert>
+        <h1>Hello</h1>
+      </Alert>
+    }
   </div>
 }
 
---
+++ src/modules/Alert/views/Alert.tsx
@@ -0,0 +1,15 @@
+import { ReactNode } from "react"
+
+interface Props {
+  children: ReactNode
+}
+
+const Alert = ({ children }: Props) => {
+  return (
+    <div className="alert alert-primary" role="alert">
+      {children}
+    </div>
+  )
+}
+
+export default Alert
